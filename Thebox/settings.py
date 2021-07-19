8f7472f974929803782686deda628cf69e40500a
soul <celestiallie@gmail.com>
2021-07-19 14:05:26 +0800

    did some work

--- Thebox/settings.py
+++ Thebox/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tools',
+    'sass_processor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,11 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'scss/')
 
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- test.py
+++ test.py
@@ -7,3 +7,4 @@ aaa = re.sub('\d\d\d', str)
 print(aaa)
 
 # print(result)
+

de5f02216c5f6752252a0782ac222704a98cfae8
soul-pc <celestiallie@gmail.com>
2021-07-19 21:26:58 +0800

    bashtest

--- z.sh
+++ z.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    git add . 
-    git commit -m "fast commit"
-    git push origin main
-else
-    git add . 
-    git commit -m $1
-    git push origin $2
-fi
+# if [ $1 == 'activate' ]; then
+#     source ./venv/Scripts/activate
+#     exit 1
+# fi
 
-echo 'Push successfully !'
+# if [ $1 == 'run' ]; then
+#     py manage.py runserver
+#     exit 1
+# fi
+case $1 in
+        v ) 
+            source ./venv/Scripts/activate
+            exit 1
+            ;;
+        run ) 
+            py manage.py runserver
+            exit 1
+            ;;
+        -m ) 
+            git add . 
+            git commit -m $2
+            git push origin $3
+            exit 1
+            ;;
+esac
+
+git add . 
+git commit -m "fast commit"
+git push origin main
+
+# if [ $# -eq 0 ]; then
+#     git add . 
+#     git commit -m "fast commit"
+#     git push origin main
+# else
+#     git add . 
+#     git commit -m $1
+#     git push origin $2
+# fi
+
+# echo 'Push successfully !'
 
 # while test $# -gt 0; do
 #       case "$1" in

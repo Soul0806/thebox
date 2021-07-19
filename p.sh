91fa2ebbeecd1d700b7359fe52e96b6ce3fd654a
soul-pc <celestiallie@gmail.com>
2021-07-19 18:08:46 +0800

    fast commit

--- p.sh
+++ p.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
-while test $# -gt 0; do
-      case "$1" in
-          -i)
-              # shift
-              first_argument=$1
-              shift
-              ;;
-          -a)
-              # shift
-              second_argument=$1
-              shift
-              ;;
-          *)
-              echo "$1 is not a recognized flag!"
-              exit
-              ;;
-      esac
-done 
-# git add . 
-# git commit -m $1
-# git push origin $2
 
+if [ $# -eq 0 ]; then
+    git add . 
+    git commit -m "fast commit"
+    git push origin main
+else
+    git add . 
+    git commit -m $1
+    git push origin $2
+fi
 
-echo 'first' + $first_argument
-echo $second_argument
-# echo 'Push sucessfully'
+echo 'Push successfully'
+
+# while test $# -gt 0; do
+#       case "$1" in
+#           -i)
+#               shift
+#               first_argument=$1
+#               shift
+#               ;;
+#           *)
+#               echo "$1 is not a recognized flag!"
+#               exit 1
+#               ;;
+#       esac
+# done 

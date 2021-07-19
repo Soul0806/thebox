0f3f2728c72aba141806fddfa9449247ebf324f7
soul <celestiallie@gmail.com>
2021-07-19 17:10:36 +0800

    did some work

--- p.sh
+++ p.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
+while test $# -gt 0; do
+      case "$1" in
+          -i)
+              # shift
+              first_argument=$1
+              shift
+              ;;
+          -a)
+              # shift
+              second_argument=$1
+              shift
+              ;;
+          *)
+              echo "$1 is not a recognized flag!"
+              exit
+              ;;
+      esac
+done 
+# git add . 
+# git commit -m $1
+# git push origin $2
 
-git add . 
-git commit -m $1
-git push origin $2
 
-echo 'Push sucessfully'
+echo 'first' + $first_argument
+echo $second_argument
+# echo 'Push sucessfully'

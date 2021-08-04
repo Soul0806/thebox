b21959618716d8d794cd5a5401271b993bab4714
soul-pc <celestiallie@gmail.com>
2021-08-04 21:59:00 +0800

    commit

--- z.sh
+++ z.sh
@@ -32,9 +32,6 @@ case $1 in
             git add . 
             git commit -m "fast commit~"
             git push origin main
-            if [ $? ] 
-               then echo 123
-            fi
             exit 1
             ;;
         test ) 

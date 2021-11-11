e51efe7cec19d86484909d5a9e6186972dd61a97
soul <celestiallie@gmail.com>
2021-11-11 13:47:23 +0800

    dev

--- task/models.py
+++ task/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.db.models.fields import CharField, TextField
+from django.db.models.fields import CharField, TextField, IntegerField
 # Create your models here.
 
 class Sort(models.Model): 
   item = models.CharField(max_length=64)
+  order = models.IntegerField()
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
--- task/static/task/js/test.js
+++ task/static/task/js/test.js
@@ -1,7 +1,13 @@
 // Make the DIV element draggable:
-dragElement(document.getElementById("div1"));
-dragElement(document.getElementById("div2"));
-dragElement(document.getElementById("div3"));
+// dragElement(document.getElementById("div0"));
+// dragElement(document.getElementById("div1"));
+// dragElement(document.getElementById("div2"));
+
+let sortElmnts = document.querySelectorAll('.sort__item');
+sortElmnts.forEach((elmnt, index) => {
+  dragElement(elmnt);
+  elmnt.style.top = (index * 100) + 'px';
+})
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -24,35 +30,95 @@ function dragElement(elmnt) {
     document.onmousemove = elementDrag;
   }
 
-  let order = elmnt.dataset.sort;
+
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
+    // console.log(e.target.style.top, e.target.offsetTop);
+
+    e.target.style['z-index'] = '9999';
+
     // calculate the new cursor position:
     // pos1 = pos3 - e.clientX;
     // pos3 = e.clientX;
     pos2 = pos4 - e.clientY;
     pos4 = e.clientY;
+    let sort = parseInt(e.target.dataset.sort, 10);
+    let prvs = sort - 1;
+    let next = sort + 1;
+    // const previousElmnt = document.querySelector(`#div${parseInt(sort - 1)}`);
+    // const nextElmnt = document.querySelector(`#div${parseInt(sort + 1)}`);
+    const previousElmnt = document.querySelector(`[data-sort="${prvs}"]`);
+    const nextElmnt = document.querySelector(`[data-sort="${next}"]`);
 
-    if (elmnt.offsetTop != 0 && elmnt.offsetTop % 80 == 0) {
-      console.log(order++);
+    // console.log(sort, previousElmnt, `#div${sort++}`);
+    // let offset = elmnt.offsetTop - start;
+    // const previousCritial
+
+    if (previousElmnt != null) {
+      const previousCritial = previousElmnt.offsetTop + previousElmnt.offsetHeight;
+      // const nextCtitical = nextElmnt.offsetTop;
+
+      if (e.target.offsetTop <= previousCritial) {
+        let move = (previousElmnt.offsetTop + 100) + 'px';
+        previousElmnt.style.top = (previousElmnt.offsetTop + 100) + 'px';
+        previousElmnt.dataset.sort++;
+        e.target.dataset.sort--;
+        sort--;
+      }
+      // // console.log(e.target.offsetTop, `#div${sort + 1}`);
     }
+    // console.log(`#div${parseInt(sort + 1)}`, nextElmnt);
+    if (nextElmnt != null) {
+      // const previousCritial = previousElmnt.offsetTop + previousElmnt.offsetHeight;
+      const nextCtitical = nextElmnt.offsetTop;
+      if (e.target.offsetTop >= nextCtitical) {
+        // console.log(previousElmnt.offsetTop + '100px');
+        console.log(e.target.offsetTop, nextCtitical);
+        nextElmnt.style.top = (nextElmnt.offsetTop - 100) + 'px';
+        nextElmnt.dataset.sort--;
+        e.target.dataset.sort++;
+        sort++;
+      }
+    }
+
+    // console.log(previousCritial);
+
+    // const nextCritial =
+    // console.log(offset);
+    // console.log(Math.floor(offset / 80));
+    // if (Math.floor(offset / 60) == base + 1) {
+    //   order++;
+    //   let target = document.querySelector(`#div${order}`);
+    //   console.log(target.offsetTop);
+    //   target.style.top = (target.offsetTop - 100) + 'px';
+    //   base += 1;
+    // } else {
+    // order--;
+    // let target = document.querySelector(`#div${order}`);
+    // console.log(target.offsetTop);
+    // target.style.top = (target.offsetTop + 100) + 'px';
+    // base -= 1;
+    // // document.querySelector(`#div${order}`).style.top += '100';
+    // base -= 1;
+
     // let rect = e.target.getBoundingClientRect();
     // console.log(rect.top + window.scrollY) 
     // set the element's new position:
+    if (e.target.offsetTop > 210) {
+
+    }
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+
     // elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
-  function closeDragElement() {
+  function closeDragElement(e) {
     // stop moving when mouse button is released:
+    let sort = e.target.dataset.sort;
+    e.target.style.top = (sort - 1) * 100 + 'px';
+    e.target.style['z-index'] = 'initial';
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
-
-const ele = document.querySelectorAll('.drag');
-
-ele.forEach((e, index) => {
-  e.style.top = (index * 100) + 'px';
-})

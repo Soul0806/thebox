8409819f2b426519ea823d5e089c258f019bb2b6
soul <celestiallie@gmail.com>
2021-11-12 15:37:42 +0800

    dev

--- task/static/task/js/test.js
+++ task/static/task/js/test.js
@@ -1,8 +1,3 @@
-// Make the DIV element draggable:
-// dragElement(document.getElementById("div0"));
-// dragElement(document.getElementById("div1"));
-// dragElement(document.getElementById("div2"));
-
 let sortElmnts = document.querySelectorAll('.sort__item');
 sortElmnts.forEach((elmnt, index) => {
   dragElement(elmnt);
@@ -11,14 +6,8 @@ sortElmnts.forEach((elmnt, index) => {
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
+  // otherwise, move the DIV from anywhere inside the DIV:
+  elmnt.onmousedown = dragMouseDown;
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
@@ -34,49 +23,34 @@ function dragElement(elmnt) {
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // console.log(e.target.style.top, e.target.offsetTop);
-
     e.target.style['z-index'] = '1';
-
     // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos3 = e.clientX;
+    // pos1 = pos3 - e.clientX;
+    // pos3 = e.clientX;
     pos2 = pos4 - e.clientY;
     pos4 = e.clientY;
     let sort = parseInt(e.target.dataset.sort, 10);
     let prvs = sort - 1;
     let next = sort + 1;
-    // const previousElmnt = document.querySelector(`#div${parseInt(sort - 1)}`);
-    // const nextElmnt = document.querySelector(`#div${parseInt(sort + 1)}`);
     const previousElmnt = document.querySelector(`[data-sort="${prvs}"]`);
     const nextElmnt = document.querySelector(`[data-sort="${next}"]`);
 
-    // console.log(sort, previousElmnt, `#div${sort++}`);
-    // let offset = elmnt.offsetTop - start;
-    // const previousCritial
-
     if (previousElmnt != null) {
       const previousCritial = previousElmnt.offsetTop + previousElmnt.offsetHeight;
-      // const nextCtitical = nextElmnt.offsetTop;
-
       if (e.target.offsetTop <= previousCritial) {
         let move = (previousElmnt.offsetTop + 100) + 'px';
-        previousElmnt.style.top = (previousElmnt.offsetTop + 100) + 'px';
+        previousElmnt.style.top = move;
         previousElmnt.dataset.sort++;
         e.target.dataset.sort--;
         sort--;
       }
-      // // console.log(e.target.offsetTop, `#div${sort + 1}`);
     }
-    // console.log(`#div${parseInt(sort + 1)}`, nextElmnt);
     if (nextElmnt != null) {
-
-      // const previousCritial = previousElmnt.offsetTop + previousElmnt.offsetHeight;
+      console.log(e.target.offsetTop);
       const nextCtitical = nextElmnt.offsetTop;
       if (e.target.offsetTop > nextCtitical) {
-        console.log('active');
-        // console.log(e.target.offsetTop, nextCtitical);
-        nextElmnt.style.top = (nextElmnt.offsetTop - 100) + 'px';
+        let move = (nextElmnt.offsetTop - 100) + 'px';
+        nextElmnt.style.top = move;
         nextElmnt.dataset.sort--;
         e.target.dataset.sort++;
         sort++;
@@ -107,15 +81,13 @@ function dragElement(elmnt) {
     // console.log(rect.top + window.scrollY) 
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement(e) {
     // stop moving when mouse button is released:
+    e.target.style['z-index'] = 'initial';
     let sort = e.target.dataset.sort;
     e.target.style.top = (sort - 1) * 100 + 'px';
-    e.target.style['z-index'] = 'initial';
     document.onmouseup = null;
     document.onmousemove = null;
   }

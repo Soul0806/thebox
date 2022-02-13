eda007c1ed76eb3d713fe503fe95eebb31274a57
Soul <celestiallie@gmail.com>
2022-02-13 21:31:12 +0800

    dev

--- Thebox/settings.py
+++ Thebox/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'scss/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATICFILES_FINDERS = [
